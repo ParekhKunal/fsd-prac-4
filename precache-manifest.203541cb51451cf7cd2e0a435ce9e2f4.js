efb3bf906b98ebac30e94c8e9f52713678e19bee
parekhkunal <parekhkunal09@gmail.com>
2022-08-19 18:14:09 +0530

    Updates

---
+++ precache-manifest.203541cb51451cf7cd2e0a435ce9e2f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd40e89c476877a5fd97c9c470192350",
+    "url": "/fsd-prac-4/index.html"
+  },
+  {
+    "revision": "7108eb0c4b14536ab2ff",
+    "url": "/fsd-prac-4/static/css/main.ee5d6995.chunk.css"
+  },
+  {
+    "revision": "58e9e32489f205a49016",
+    "url": "/fsd-prac-4/static/js/2.a6507c3f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/fsd-prac-4/static/js/2.a6507c3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7108eb0c4b14536ab2ff",
+    "url": "/fsd-prac-4/static/js/main.1eb58547.chunk.js"
+  },
+  {
+    "revision": "1dd7a6053ff0326e5131",
+    "url": "/fsd-prac-4/static/js/runtime-main.31a8cc17.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/fsd-prac-4/precache-manifest.203541cb51451cf7cd2e0a435ce9e2f4.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fsd-prac-4/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
